5a275f9b1cebd2f9722309157eccbf256582a28d
jsee-twilio <ychian@gmail.com>
2020-10-23 22:18:29 +0800

    express

---
+++ src/app.js
@@ -0,0 +1,37 @@
+
+const path = require('path')
+const express = require('express')
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(path.join(__dirname, '../public'))
+
+app.use(express.static(publicDirectoryPath))
+
+// app.get('', (req, res)=>{
+//     res.send('Hello Express!')
+// })
+
+// app.get('/help', (req, res)=>{
+//     res.send({
+//         name: 'Andrew',
+//         age: 27
+//     })
+// })
+
+// app.get('/about', (req, res)=>{
+//     res.send('<h1>About</h1>')
+// })
+
+app.get('/weather', (req, res)=>{
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
