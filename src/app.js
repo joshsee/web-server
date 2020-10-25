5f8c4177fe3705f14e4f332d34e7b8aa377c0c30
jsee-twilio <ychian@gmail.com>
2020-10-25 20:05:50 +0800

    Create weather endpoint

--- src/app.js
+++ src/app.js
@@ -47,9 +47,28 @@ app.use(express.static(publicDirectoryPath))
 // })
 
 app.get('/weather', (req, res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: 'Address must be provided'
+        })
+        return
+    }
     res.send({
         forecast: 'It is snowing',
-        location: 'Philadelphia'
+        location: 'Hong Kong',
+        address: req.query.address
+    })
+})
+
+app.get('/product', (req, res)=>{
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query)
+    res.send({
+        product: []
     })
 })
 
