f8543c7045d3e0cd58e880a6c3ff46d705d1d9eb
jsee-twilio <ychian@gmail.com>
2020-10-26 13:08:15 +0800

    create fetch api

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 console.log('Print something')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+fetch('http://localhost:3000/weather?address=hong%20kong').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
