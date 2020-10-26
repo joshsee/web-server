11bcaa2060ad2ee7c183dcd45e9275008fdf3350
jsee-twilio <ychian@gmail.com>
2020-10-26 20:18:47 +0800

    Add humidity message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longtitude, callback) => {
             callback('Unable to find location. Try another search', undefined)
         } else {
             // console.log(response.body.location.name)
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently "+ body.current.temperature +" degrees out. It feels like "+ body.current.feelslike + " degrees out")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently "+ body.current.temperature +" degrees out. It feels like "+ body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + ".")
         }
     })
 }
